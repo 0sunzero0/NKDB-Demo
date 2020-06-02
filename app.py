2a3bfee304a1caecaee31ca88c7886bb9ac3e243
0sunzero0 <21500209@handong.edu>
2020-06-03 01:04:34 +0900

    final version

--- app.py
+++ app.py
@@ -1,43 +1,38 @@
 from elasticsearch import Elasticsearch
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
 # Url address of Elasticsearch
-#localUrl = "http://localhost:9200"
-serverUrl = "http://203.252.117.201:9200"
-INDEX = "nkdb"
+localUrl = "http://localhost:9200"
+#serverUrl = "http://203.252.117.201:9200"
+INDEX = "nkdb200531"
 
 # Elasticsearch Connection
-es = Elasticsearch(serverUrl)
-#es = Elasticsearch(localUrl)
+#es = Elasticsearch(serverUrl)
+es = Elasticsearch(localUrl)
 app = Flask(__name__)
 
 
 @app.route('/')
-def index(keyword=None):
-    return render_template('index.html', keyword=keyword)
+def index():
+    return render_template('index.html')
 
 @app.route('/result', methods=['POST'])
-def result(keyword=None):
+def result():
     total = 10
     temp_query = request.form['keyword']
     print(temp_query)
     corpus = elasticsearchGetDocs(total, temp_query)
     return render_template('result.html', docs=corpus)
 
-# @app.route('/content', methods=['POST'])
-# def content(oneDoc=None):
-#     #oneDoc = request.doc
-#     return render_template('content.html')
-
 # make query to transmit to elasticsearch
 # input: isFile, select size[0, infinity]
 # output: es query body(object)
 def transmitQuery(size, temp_query):
     doc = {}
-    if size > 0:
-        doc['size'] = size
+    doc['size'] = size
 
     doc['query'] = {
         "query_string":{
@@ -56,7 +51,6 @@ def elasticsearchCount(doc):
     count = count['count']
     return count
 
-
 # return data
 # input: es query body
 # output: json format data (object)
@@ -67,10 +61,11 @@ def elasticsearchQuery(doc):
 # return documents
 # input: count of document to return (int)
 # output: document (object array)
-
 def nkdbContent(SIZE, temp_query):
     doc = transmitQuery(SIZE, temp_query)
-    result = elasticsearchQuery(doc)
+    temp_result = elasticsearchQuery(doc)
+
+    result = temp_result["hits"]["hits"]
     num = len(result)
     print("전달 받은 문서의 수 : ", num)
 
@@ -110,6 +105,7 @@ def nkdbContent(SIZE, temp_query):
 def elasticsearchGetDocs(total, temp_query):
     corpus = []
     data = nkdbContent(10, temp_query)
+
     # print(data)
     for oneDoc in data:
         corpus.append(oneDoc)
@@ -118,7 +114,7 @@ def elasticsearchGetDocs(total, temp_query):
     return corpus
 
 if __name__ == '__main__':
-    #app.run()
     app.run(host="0.0.0.0",port=5000, debug=True)
+
 
 
---
+++ debug.py
@@ -0,0 +1,61 @@
+from elasticsearch import Elasticsearch
+
+URL = "http://localhost:9200"
+INDEX = "nkdb200529"
+
+es = Elasticsearch(URL)
+
+
+def getResult(temp_query):
+    query_doc = {}
+    query_doc['query'] = {
+        "multi_match": {
+            "query": temp_query,
+            "fields": ["post_title", "post_body", "file_name", "file_extracted_content"],
+            "tie_breaker": 0.2,
+            "type": "best_fields"
+        }
+    }
+    print(query_doc)
+    result = es.search(index=INDEX, body=query_doc)
+    #print(result)
+    rawData = result["hits"]["hits"]
+
+    length = len(rawData)
+    print("###### es 보내준 문서의 갯수 : ", length)
+
+    final_result = []
+
+    for doc in rawData:
+        if "file_extracted_content" in doc:
+            final_result.append(
+                {
+                    "_id": doc["_id"],
+                    "post_title":doc["_source"]["post_title"],
+                    "content": doc["_source"]["file_extracted_content"],
+                    "file_name": doc["_source"]["file_name"],
+                    "file_url": doc["_source"]["file_download_url"],
+                    "post_date": doc["_source"]["post_date"],
+                    "post_writer": doc["_source"]["post_writer"],
+                    "published_institution_url": doc["_source"]["published_institution_url"],
+                    "top_category": doc["_source"]["top_category"]
+                }
+            )
+        else:
+            final_result.append(
+                {
+                    "_id": doc["_id"],
+                    "post_title": doc["_source"]["post_title"],
+                    "content": doc["_source"]["post_body"],
+                    "post_date": doc["_source"]["post_date"],
+                    "post_writer": doc["_source"]["post_writer"],
+                    "published_institution_url": doc["_source"]["published_institution_url"],
+                    "top_category": doc["_source"]["top_category"]
+                }
+            )
+    print(final_result)
+
+    return final_result
+
+temp_query = "김정은"
+result_list = getResult(temp_query)
