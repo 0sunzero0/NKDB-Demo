a59343dc2a32a3d98c593d7d839d9f739b1be904
0sunzero0 <21500209@handong.edu>
2020-06-06 21:08:27 +0900

    revise hlight.py

--- hlight.py
+++ hlight.py
@@ -1,16 +1,56 @@
 import re
 
 def hlight_term(string, term):
-    if len(term) < 2:
-        idx = string.find(term[0])
-        begin = max(18,idx-10)
-        pattern = "r'("+term[0]+")'"
-        result = string[begin:begin+500]
+    term = term.split()
+
+    if str(type(string)) == "<class 'NoneType'>":
+        result = " "
+        return result
+
+    if len(string) < 300:
+        return string
+
+    elif len(string) < 600:
+        if len(term) < 2:
+            idx = string.find(term[0])
+            if idx < 10:
+                begin = idx
+                result = string[begin:begin + 300]
+                result = result + " ..."
+            elif idx < 300:
+                begin = idx - 10
+                result = string[begin:begin + 300]
+                result = "... " + result + " ..."
+            else:
+                begin = 300
+                result = string[begin:begin + 300]
+                result = "... " + result
+        else:
+            begin = 300
+            for i in range(len(term)):
+                begin = min(begin-10, string.find(term[i]))
+            result = string[begin:begin + 300]
+            result = result + " ..."
         return result
     else:
-        begin = 400
-        for i in range(len(term)):
-            begin = min(begin,string.find(term[i]))
-        begin = max(18,begin)
-        result = string[begin:begin+500]
+        if len(term) < 2:
+            idx = string.find(term[0])
+            if idx < 10:
+                begin = idx
+                result = string[begin:begin + 300]
+                result = result + " ..."
+            elif idx < len(string)-300:
+                begin = idx-10
+                result = string[begin:begin + 300]
+                result = "... " + result + " ..."
+            else:
+                begin = len(string)-300
+                result = string[begin:begin + 300]
+                result = "... " + result
+        else:
+            begin = len(string)-300
+            for i in range(len(term)):
+                begin = min(begin-10, string.find(term[i]))
+            result = string[begin:begin+300]
+            result = result + " ..."
         return result
