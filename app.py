6c0cbe77b28f207b58fba9d7a2a91fa41c7ef7c2
0sunzero0 <21500209@handong.edu>
2020-06-08 00:20:09 +0900

    revise app.py for contracted url

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import json
 import search
-
+import re
 
 app = Flask(__name__)
 
@@ -23,11 +23,18 @@ def result():
 @app.route('/content/<docID>', methods=['GET'])
 def content(docID):
     result_list = search.es.get(index=INDEX, id=docID)['_source']
+    ### for contracted url ###
+    url_list = re.findall("((http(s)?:\/\/)([a-z0-9\w]+\.*)+[a-z0-9]{2,4})", result_list['published_institution_url'])
+    contracted_url = url_list[0][0]
+    ###
     if isinstance(result_list['post_body'], str):
         result_list['post_body'] = result_list['post_body'].strip()
-    if str(type(result_list['post_body'])) == "<class 'NoneType'>":
+    if result_list['post_body'] is None:
         del result_list['post_body']
-    return render_template('content.html', doc=result_list)
+    #return render_template('content.html', doc=result_list)
 
+    ### for contracted url ###
+    return render_template('content.html', doc=result_list, contracted_url = contracted_url)
+    ###
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000, debug=True)
